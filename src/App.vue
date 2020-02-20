bc63d80385494244cfe4d8d9036e2be3a23da8ac
Elizabeth Villalejos <nekobanpaia@gmail.com>
2020-02-20 08:58:21 -0700

    Adding VueFullPage to main and app

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,37 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <full-page :options="options" id="fullpage">
+        <div class="section">
+            <h3>Section 1</h3>
+        </div>
+        <div class="section">
+            <h3>Section 2</h3>
+        </div>
+    </full-page>  
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  // components: {
+  //   HelloWorld
+  // }
+  data () {
+      return {
+        options: {
+          licenseKey: 'YOUR_KEY_HERE',
+          afterLoad: this.afterLoad,
+          scrollOverflow: true,
+          scrollBar: false,
+          menu: '#menu',
+          navigation: true,
+          anchors: ['page1', 'page2', 'page3'],
+          sectionsColor: ['#41b883', '#ff5f45', '#0798ec', '#fec401', '#1bcee6', '#ee1a59', '#2c3e4f', '#ba5be9', '#b4b8ab']
+        }
+      }
+    }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueFullPage from 'vue-fullpage.js'
+
+Vue.use(VueFullPage)
 
 Vue.config.productionTip = false
 
